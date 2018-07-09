7e080043cb4818971da73a3c136616d80805d3ae
dantejf88 <dantejf88@gmail.com>
2018-07-09 15:37:41 -0300

    CssNotebook terminado

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import getZen from "./api/fetch-zen"
 import { RingLoader } from 'react-spinners'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 import { Button, ButtonToolbar } from "react-bootstrap"
-import Phrase from "./components/phrase"
+import Phrase from "./component/phrase"
 import "./App.css"
 
-let sentence = []
+let sentence = [
+  "Mind your words, they are important.","Mind your words, they are important.","Mind your words, they are important.","Mind your words, they are important.","Mind your words, they are important.","Mind your words, they are important.","Mind your words, they are important.","Mind your words, they are important.","Mind your words, they are important.","Mind your words, they are important.","Mind your words, they are important.","Mind your words, they are important.","Mind your words, they are important."
+]
 let i = -1
 class App extends Component {
   constructor(props) {
@@ -79,13 +81,12 @@ class App extends Component {
         {!this.state.loading &&
           <div className="bigContainer">
             {this.state.limit &&
-              <div>
+              <div className="limit">
                 <p>That is all the wisdom you need, remember:</p>
                 <p>Anything added dilutes everything else</p>
               </div>
             }
-            <div className="midleContainer">
-
+            <div className="buttonsContainer">
               {i >= 1 &&
                 <div  className="previous">
                   <ButtonToolbar>
@@ -93,49 +94,52 @@ class App extends Component {
                       onClick={this.previous}
                       disabled={this.state.loading}
                       bsSize="large">
-                      Previous Idea</Button>
+                      Previous</Button>
                   </ButtonToolbar>
                 </div>
               }
 
-              <div className="litleContainer">
+              {!(i === sentence.length-1) &&
+                <div  className="next">
+                  <ButtonToolbar>
+                    <Button
+                      onClick={this.next}
+                      disabled={this.state.loading}
+                      bsSize="large">
+                      Next</Button>
+                  </ButtonToolbar>
+
+                </div>
+              }
+            </div>
+            <div className="middleContainer">
+              <div className="transitionContainer">
                 <ReactCSSTransitionGroup
                   transitionName="fade"
-                  transitionAppear={true}
-                  transitionAppearTimeout={500}
-                  transitionEnterTimeout={3000}
-                  transitionLeaveTimeout={3000}
+                  transitionEnterTimeout={500}
+                  transitionLeaveTimeout={500}
                   >
                   <Phrase
+                    className="phraseContainer"
                     texto={this.state.texto}
                     index={i}
                     key={this.state.texto}
                     />
                 </ReactCSSTransitionGroup>
               </div>
-              {!(i === sentence.length-1) &&
-                <div>
-                  <ButtonToolbar className="next">
-                    <Button
-                      onClick={this.next}
-                      disabled={this.state.loading}
-                      bsSize="large">
-                      Next Idea</Button>
-                  </ButtonToolbar>
-                </div>
-              }
-            </div>
 
               <div  className="more">
                 <ButtonToolbar>
                   <Button
-                    block
+
                     onClick={this.new}
                     disabled={this.state.loading}
                     bsSize="large">
-                  More wisdom</Button>
+                    More wisdom</Button>
                 </ButtonToolbar>
               </div>
+
+            </div>
 
           </div>
       }
